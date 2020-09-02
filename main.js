7e812e7661a2204de9e6b5ce1180279319afb04c
Xavier515 <66922742+Xavier515@users.noreply.github.com>
2020-09-01 23:21:35 -0500

    First

---
+++ main.js
@@ -0,0 +1,40 @@
+const Discord = require('discord.js');
+
+const client = new Discord.Client();
+
+const prefix ='!';
+var events = "1st General Meeting";
+
+client.once('ready',() => {
+    console.log('task_bot is online!');
+});
+
+client.on('message',message =>{
+if (!message.content.startsWith(prefix)  || message.author.bot ) return;
+
+const args = message.content.slice(prefix.length).split(/ +/);
+const command = args.shift().toLowerCase();
+
+
+if (command == 'task'){
+    message.channel.send('None tasks are set!');
+}
+else if(command == 'youtube')
+{
+    message.channel.send('Go and suscribe to: https://www.youtube.com/channel/UC1bYosSS-J8LNkQo2ozeQXw');
+}
+else if(command == 'events')
+{
+    if (events == ' ') 
+    message.channel.send('No events for now💤');
+    else 
+    message.channel.send(events);
+}
+
+});
+
+
+
+
+
+client.login('process.env.token');
